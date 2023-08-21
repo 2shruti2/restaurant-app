5b5fb4a9fab14f1aa9d11b3ca10be0f4bd0112ed
Shruti <shruthesharma@gmail.com>
2023-08-22 00:14:27 +0530

    fixed button

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => (
         <h1 className='headtext__cormorant'>About Us</h1>
         <img src={images.spoon} alt='spoon img' className='spoon__img'/>
         <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type='button' className=''>Know More</button>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
 
       <div className='app__aboutus-content_knife flex__center blur__load' loading='lazy' style={{backgroundImage: `url(${images.knifeSmall})` }} > 
